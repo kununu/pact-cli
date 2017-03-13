adedd83cb0085a23b2e2db2c872f388b05cd067f
Andreas Prummer <andreas.prummer@kununu.com>
2017-03-13 11:00:16 +0100

    Add version helper class

--- src/pactBrokerHelper.js
+++ src/pactBrokerHelper.js
@@ -1,5 +1,36 @@
+import url from 'url';
+import path from 'path';
+
 const request = require('request');
 
-export function requestUrlFromBroker(endpoint) {
+export function getVersionForPact(consumer, provider) {
+  const brokerEndpoint = getBrokerEndpoint(
+    'consumer-provider',
+    {
+      consumer,
+      provider
+    }
+  );
+
+  return requestUrlFromBroker(brokerEndpoint)
+    .then((response) => {
+      // return the current latest version
+      return path.basename(
+        url.parse(response._links.self.href).pathname
+      );
+    });
+}
+
+export function getBrokerEndpoint(type, options) {
+  switch (type) {
+    case 'consumer-provider':
+      return `/pacts/provider/${options.provider}/${options.consumer}/latest`;
+      break;
+    default:
+      throw new Error('not definied endpoint type');
+  }
+}
+
+function requestUrlFromBroker(endpoint) {
   return request(endpoint);
 }
--- src/pactBrokerHelper.test.js
+++ src/pactBrokerHelper.test.js
@@ -1,15 +1,32 @@
-import {requestUrlFromBroker} from './pactBrokerHelper';
+import {getVersionForPact, getBrokerEndpoint} from './pactBrokerHelper';
 
-const mockedResponse = {
+describe('version handling based on pact', () => {
+  test('get version for pact provider consumer latest pact', () => {
+    // set mock scenario used by /__mocks__/request.js
+    global.MOCK_REQUEST_SCENARIO = 'broker-latest-pact';
+    const
+      consumer = 'www',
+      provider = 'users-service';
 
-}
-
-jest.mock('request', () => (
-  jest.fn(() => 'pseudo store object')
-));
-
-describe('request to pact-broker', () => {
-  it('should request with given url', () => {
-    expect(requestUrlFromBroker('http://127.0.0.1:5000/pacts')).toBe('pseudo store object');
+    const expectedVersion = '1.0.0';
+    return getVersionForPact(consumer, provider)
+      .then(version => expect(version).toBe(expectedVersion));
   });
-})
+});
+
+describe('creation for pact-brokern endpoints', () => {
+  test('undefined type throws error', () => {
+    expect(() => {
+      getBrokerEndpoint('not-given', {})
+    }).toThrow();
+  });
+
+  test('get latest endpoint base on consumer and provider', () => {
+    const options = {
+      consumer: 'test-consumer',
+      provider: 'test-provider',
+    };
+
+    expect(getBrokerEndpoint('consumer-provider', options)).toBe('/pacts/provider/test-provider/test-consumer/latest');
+  });
+});
