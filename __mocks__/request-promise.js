d226afe85818f8ba6697687b86397fc8f3354c48
Andreas Prummer <andreas.prummer@kununu.com>
2017-04-14 07:53:09 +0200

    Remove not needed return and fix linting

--- __mocks__/request-promise.js
+++ __mocks__/request-promise.js
@@ -1,47 +1,46 @@
 import {getConfig} from '../src/__mocks__/helpers';
 
+function getMatchUri (tag) {
+  return `${getConfig().brokerUrl}/pacts/provider/test-provider/consumer/test-consumer/latest` // eslint-disable-line
+  + (tag ? `/${tag}` : '');
+}
 
-function request(url) {
+function request (url) {
   // TODO get rid of those globals
   switch (global.MOCK_REQUEST_SCENARIO) {
     case 'broker-latest-pact':
-        // TODO: consider whether this should be tested here?
-        expect(url).toMatchObject({
-          json: true,
-          uri: getMatchUri(global.MOCK_REQUEST_TAG)
-        });
-        return new Promise((resolve, reject) => {
-          resolve(require('./data/latest-tag-pact.json'));
-        });
+      // TODO: consider whether this should be tested here?
+      expect(url).toMatchObject({
+        json: true,
+        uri: getMatchUri(global.MOCK_REQUEST_TAG),
+      });
+      return new Promise((resolve, reject) => { // eslint-disable-line
+        resolve(require('./data/latest-tag-pact.json')); // eslint-disable-line
+      });
     case 'broker-rc-pact':
       expect(url).toMatchObject({
         json: true,
-        uri: getMatchUri(global.MOCK_REQUEST_TAG)
+        uri: getMatchUri(global.MOCK_REQUEST_TAG),
       });
-      return new Promise((resolve, reject) => {
-        resolve(require('./data/rc-pact.json'));
+      return new Promise((resolve, reject) => { // eslint-disable-line
+        resolve(require('./data/rc-pact.json')); // eslint-disable-line
       });
     case 'subsequent-requests-feature-master':
       expect(url).toMatchObject({
         json: true,
-        uri: getMatchUri(global.MOCK_REQUEST_TAG)
+        uri: getMatchUri(global.MOCK_REQUEST_TAG),
       });
       global.MOCK_REQUEST_SCENARIO = 'broker-latest-pact';
       global.MOCK_REQUEST_TAG = 'master';
       return new Promise((resolve, reject) => {
         reject({
-          statusCode: 404
+          statusCode: 404,
         });
       });
     default:
       throw new Error('undefined scenario');
   }
-  return 'response';
 }
 
-function getMatchUri(tag) {
-  return  `${getConfig().brokerUrl}/pacts/provider/test-provider/consumer/test-consumer/latest`
-  + (tag ? `/${tag}` : '');
-}
 
 module.exports = request;
